65a879751a8f8f703c873124503b4803985af826
codewarnab <dp950810@gmail.com>
2025-02-27 00:21:44 +0530

    removed redirecttion

--- next.config.ts
+++ next.config.ts
@@ -12,15 +12,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['img.clerk.com'], //allows images to be loaded from the specified domain
   },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/landing',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
