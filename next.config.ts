c8a990b98f44b0366a28b02ccf0eb2a3b5804022
Dhruba Bhattacharyya <ece2023114@rcciit.org.in>
2025-02-26 21:23:23 +0530

    landing page added

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,17 @@ const nextConfig: NextConfig = {
   // reactStrictMode: true,
   images: {
     domains: ['img.clerk.com'], //allows images to be loaded from the specified domain
-    },
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/landing',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
+
